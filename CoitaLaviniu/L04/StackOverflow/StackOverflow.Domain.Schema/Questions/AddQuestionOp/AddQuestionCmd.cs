f8cd1775af986b04e3ee248a479c0551a684db5c
laviniucoita <laviniu.lavi17@gmail.com>
2020-10-19 15:06:01 +0300

    lab4

---
+++ CoitaLaviniu/L04/StackOverflow/StackOverflow.Domain.Schema/Questions/AddQuestionOp/AddQuestionCmd.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace StackOverflow.Domain.Schema.Questions.AddQuestionOp
+{
+    public class AddQuestionCmd
+    {
+        public AddQuestionCmd( string title, string body, string tags)
+        {
+            Title = title;
+            Body = body;
+            Tags = tags;
+        }
+
+        [Required]
+        public string Title { get; }
+        [Required]
+        public string Body { get; }
+        [Required]
+        public string Tags { get; }
+    }
+}
